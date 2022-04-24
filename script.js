b5e24c72d8a3a43d9752d13bc0215089a016d5f5
Gabriel Viox <gviox17@gmail.com>
2022-04-24 14:50:27 -0500

    Steve came through and did a bunch of cool things ask him

--- script.js
+++ script.js
@@ -2,6 +2,28 @@
 //create an array that grabs all slider questions because we will append a string after each of them
 let sliderDescriptionArr = document.getElementsByClassName('slider-description')
 
+// Steve Range Slider Code
+// const slider = document.getElementById("myRange");
+// const output = document.getElementById("demo");
+// output.innerHTML = slider.value;
+
+// slider.oninput = function() {
+//   output.innerHTML = this.value;
+// }
+
+
+function carbonTotal(){
+    let arrayOfCarbonNumbers = document.getElementsByClassName("carbonResult");
+    let carbonTotal = document.querySelector("#carbontotal");
+    let sumTotalOfCarbonValues = 0;
+    for (let i = 0; i < arrayOfCarbonNumbers.length; i++){
+    sumTotalOfCarbonValues += parseInt(arrayOfCarbonNumbers[i].innerText);
+    }
+
+    carbonTotal.innerText = `You have offset ${sumTotalOfCarbonValues} pounds of CO2`; 
+
+} 
+
 //create an arr of the sliders
 let sliderArr = document.querySelectorAll('input')
 
@@ -10,13 +32,14 @@ function addText(arr){
     for (let i = 0; i < arr.length; i++){
         let h = document.createElement("h1")
         h.setAttribute('id', `sliderArr${[i]}Value`)
+        h.setAttribute('class', `carbonResult`)
         h.textContent = sliderArr[i].value
         arr[i].append(h)
     }
 }
 
 
-//updates value string above sider on change
+//updates value string above slider on change
 function addEventListenerToSliders(arr){
     for (let i = 0; i < arr.length; i++){
         sliderArr[i].addEventListener('change', function() {
