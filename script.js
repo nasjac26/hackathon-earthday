07af9f538b688f8391585f10529796f5f63843d0
Jacob <Jacobonasrallah@gmail.com>
2022-04-24 18:46:17 -0700

    refactored value displays and made smooth sliders

--- script.js
+++ script.js
@@ -11,20 +11,6 @@ let sliderDescriptionArr = document.getElementsByClassName('slider-description')
 //   output.innerHTML = this.value;
 // }
 
-//meat driving water flying bulbs
-function multiplyingCarbonValues() {
-    let arrayOfCarbonNumbers = document.getElementsByClassName("carbonResult");
-    //REMINDER, these multipliers are form yearly amount, so the weekly * 52
-    //all are in POUNDS by default
-    let meat = parseInt(arrayOfCarbonNumbers[0]) * 302;
-    let driving = parseInt(arrayOfCarbonNumbers[1]) * 40.56;
-    let water = parseInt(arrayOfCarbonNumbers[2]) * .468;
-    let flying = parseInt(arrayOfCarbonNumbers[3]) * .1325;
-    let bulbs = parseInt(arrayOfCarbonNumbers[4]) * 286.4;
-    let yearlyCarbonSaved = meat + driving + water + flying + bulbs;
-
-
-}
 
 function carbonTotal(){
     // let arrayOfCarbonNumbers = document.getElementsByClassName("carbonResult");
@@ -37,7 +23,6 @@ function carbonTotal(){
     //REMINDER, these multipliers are form yearly amount, so the weekly * 52
     //all are in POUNDS by default
     let meat = parseInt(arrayOfCarbonNumbers[0].innerText) * 302;
-    debugger;
     let driving = parseInt(arrayOfCarbonNumbers[1].innerText) * 40.56;
     let water = parseInt(arrayOfCarbonNumbers[2].innerText) * .468;
     let flying = parseInt(arrayOfCarbonNumbers[3].innerText) * .1325;
@@ -53,21 +38,21 @@ function carbonTotal(){
 } 
 
 //create an arr of the sliders
-let sliderArr = document.querySelectorAll('input')
+let sliderArr = document.getElementsByClassName('slider')
 
 //Script to add h1 value to each TODO: Right now it is hardcoded to starting value need ONCHANGE
-function addText(arr){
-    for (let i = 0; i < arr.length; i++){
-        let h = document.createElement("h1")
-        h.setAttribute('id', `sliderArr${[i]}Value`)
-        h.setAttribute('class', `carbonResult`)
-        h.textContent = sliderArr[i].value
-        arr[i].append(h)
-    }
-}
+// function addText(arr){
+//     for (let i = 0; i < arr.length; i++){
+//         let newH1 = document.createElement("h1");
+//         newH1.setAttribute('id', `sliderArr${[i]}Value`);
+//         newH1.setAttribute('class', `carbonResult`);
+//         newH1.textContent = sliderArr[i].value;
+//         sliderArr[i].appendChild(newH1);
+//     }
+// }
 
 
-//updates value string above slider on change
+// updates value string above slider on change
 function addEventListenerToSliders(arr){
     for (let i = 0; i < arr.length; i++){
         sliderArr[i].addEventListener('change', function() {
@@ -76,6 +61,8 @@ function addEventListenerToSliders(arr){
         })
     }
 }
+
+
 
 //DOES NOT APPEND OUTSIDE OF CONSOLE CHECK LOOM
 addText(sliderDescriptionArr)
